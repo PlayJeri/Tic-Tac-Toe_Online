2ac1ec8913440d54c8559289a66277da9804d47b
PlayJeri <numri.jeri@gmail.com>
2023-09-06 01:50:59 +0300

    home page created

---
+++ frontend/src/containers/HomePage.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavBar } from "../components/NavBar";
+import TicTacToeImage from "../../public/ttt-bg.png";
+import "../styles/HomePage.css";
+import { Link } from "react-router-dom";
+
+
+const TicTacToeBoard: React.FC = () => {
+    return (
+        <div className="ttt-board-container">
+            <img src={TicTacToeImage} alt="Tic Tac Toe Board" className="ttt-image" />
+            <Link to="/game" className="play-button">PLAY</Link>
+        </div>
+    )
+}
+
+export const HomePage: React.FC = () => {
+    return (
+        <div className="home-page">
+            <NavBar />
+            <TicTacToeBoard />
+        </div>
+    )
+}
