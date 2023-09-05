1f6596b36e00a8b2aac4efca22288bace1246de9
PlayJeri <numri.jeri@gmail.com>
2023-09-06 01:46:36 +0300

    server exporting logic changed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response} from 'express';
 import cors from 'cors';
-import { websocketController } from './websocketController';
+import { upgrade } from './websocketController';
 import { PrismaClient } from '@prisma/client';
 import authRouter from './routers/authRouter';
 
@@ -22,5 +22,5 @@ const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
 
-websocketController(server);
+upgrade(server);
 
