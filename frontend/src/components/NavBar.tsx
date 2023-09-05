e4629d2edfe8a45edc1324dbce3a9964d500d9b6
PlayJeri <numri.jeri@gmail.com>
2023-09-05 03:50:43 +0300

    Navbar created

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { LoginModal } from "./LoginModal";
+import { Link } from "react-router-dom";
+
+
+export const NavBar: React.FC = () => {
+    const [showLoginModal, setShowLoginModal] = useState(false);
+
+    const handleLoginClick = () => {
+        setShowLoginModal(true);
+    }
+
+    const handleModalClose = () => {
+        setShowLoginModal(false);
+    }
+
+
+    return(
+        <nav>
+            <div className="nav-container">
+                <div className="logo">Logo here</div>
+                <ul className="nav-links">
+                    <li><Link to="/home">Home</Link></li>
+                    <li>Profile</li>
+                </ul>
+                <div className="login-link">
+                    <button onClick={handleLoginClick}>Login</button>
+                </div>
+            </div>
+            <LoginModal isOpen={showLoginModal} onClose={handleModalClose} />
+        </nav>
+    )
+}
+
