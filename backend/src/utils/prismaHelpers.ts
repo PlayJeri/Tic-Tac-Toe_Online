c3fc145cf19fb767474502a4221bd838a0b89040
PlayJeri <numri.jeri@gmail.com>
2023-09-22 21:46:49 +0300

    added time played to query

--- backend/src/utils/prismaHelpers.ts
+++ backend/src/utils/prismaHelpers.ts
@@ -16,25 +16,33 @@ export async function getUser(username: string) {
     }
 }
 
-export async function addScores(winner: string, loser: string) {
+export async function addScores(winner: ConnectedUser, loser: ConnectedUser) {
     try {
+        const winnerTimePlayed = (Date.now() - winner.ws.connectionStartTime!) / 1000;
+        const loserTimePlayed = (Date.now() - loser.ws.connectionStartTime!) / 1000;
         await prisma.user.update({
             where: {
-                username: winner
+                username: winner.username
             },
             data: {
                 wins: {
                     increment: 1
+                },
+                timePlayedSeconds: {
+                    increment: winnerTimePlayed
                 }
             }
         });
         await prisma.user.update({
             where: {
-                username: loser
+                username: loser.username
             },
             data: {
                 losses: {
                     increment: 1
+                },
+                timePlayedSeconds: {
+                    increment: loserTimePlayed
                 }
             }
         });
