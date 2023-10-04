7862c4cc785ed9a159df352b3784e052d6bc5a5a
PlayJeri <numri.jeri@gmail.com>
2023-10-04 19:06:32 +0300

    validateLogin added

--- backend/src/routers/authRouter.ts
+++ backend/src/routers/authRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { login, register } from "../controllers/authControllers";
+import { login, register, validateLogin } from "../controllers/authControllers";
+import { validateTokenMiddleware } from "../utils/middleware";
 
 const authRouter = Router();
 
 authRouter.post('/login', login);
 authRouter.post('/register', register);
+authRouter.post('/validate', validateTokenMiddleware, validateLogin);
 
 export default authRouter;
