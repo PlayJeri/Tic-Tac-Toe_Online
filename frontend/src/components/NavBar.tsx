efe64d220927e97c1b299d9c2ee0c1fc39ca6946
PlayJeri <numri.jeri@gmail.com>
2023-10-04 19:07:42 +0300

    check log in useEffect added

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { LoginModal } from "./LoginModal";
 import { Link } from "react-router-dom";
 import "../styles/NavBar.css"
@@ -14,6 +14,29 @@ export const NavBar: React.FC = () => {
     const handleModalClose = () => {
         setShowLoginModal(false);
     }
+
+    useEffect(() => {
+        const checkIfLoggedIn = async () => {
+            const accessToken = localStorage.getItem('access_token');
+            if (!accessToken) return setShowLoginModal(true);
+
+            const headers = {
+                Authorization: `Bearer ${accessToken}`,
+                'Content-Type': "application/json",
+            };
+
+            const response = await fetch('http://localhost:3000/auth/validate', {
+                 method: 'POST',
+                 headers: headers,
+            })
+
+            if (response.status !== 200) {
+                setShowLoginModal(true);
+            }
+        }
+
+        checkIfLoggedIn();
+    }, [])
 
 
     return(
