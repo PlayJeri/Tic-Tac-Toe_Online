52bad124c5a81e43609aabc411e8204edda9936d
PlayJeri <numri.jeri@gmail.com>
2023-10-09 14:23:34 +0300

    changePassword endpoint added

--- backend/src/controllers/profileControllers.ts
+++ backend/src/controllers/profileControllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express"
-import { getUser } from "../utils/prismaHelpers";
+import { getUser, updateUserPassword } from "../utils/prismaHelpers";
 import { userData } from "../utils/types";
+import bcrypt from 'bcrypt';
 import jwt from "jsonwebtoken";
 
 
@@ -18,4 +19,29 @@ export const getProfile = async (req: Request, res: Response) => {
         losses: user.losses,
         secondsPlayed: user.timePlayedSeconds
     });
+}
+
+export const changePassword = async (req: Request, res: Response) => {
+    try {
+        const { password, newPassword } = req.body;
+        const { username } = req.decodedToken;
+    
+        const user = await getUser(username);
+        if (!user) return res.sendStatus(404);
+    
+        if (!password || !newPassword) return res.sendStatus(400);
+    
+        const verifiedPassword = await bcrypt.compare(password, user.password);
+        if (!verifiedPassword) return res.sendStatus(401);
+    
+        const newPasswordHash = await bcrypt.hash(newPassword, 10);
+        user.password = newPasswordHash
+    
+        const passwordChangedSuccessfully = await updateUserPassword(username, newPassword);
+
+        return res.sendStatus(200);
+    } catch (error) {
+        console.error("Password change failed: ", error);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
 }
