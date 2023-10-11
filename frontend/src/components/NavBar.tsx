c86a4e4c39d77c792e5df201d224eb1c405c30ae
PlayJeri <nurmi.jeri@gmail.com>
2023-10-11 15:05:54 +0300

    alerts added

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,29 +1,50 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import { LoginModal } from "./LoginModal";
+import { AlertComponent } from "./Alert";
 import "../styles/NavBar.css"
 
 
 export const NavBar: React.FC = () => {
-    return (
-        <nav className="navbar navbar-expand-lg bg-dark-subtle">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/">Home</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/profile">Profile</Link>
-                </li>
-              </ul>
-            </div>
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertVariant, setAlertVariant] = useState('');
+  const [alertMessage, setAlertMessage] = useState('');
+
+  const hideAlert = () => setShowAlert(false);
+  const showCustomAlert = (message: string, variant: string) => {
+    setAlertVariant(variant);
+    setAlertMessage(message);
+    setShowAlert(true);
+  }
+
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-dark-subtle">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/profile">Profile</Link>
+              </li>
+            </ul>
           </div>
-          <LoginModal />
-        </nav>
-      );      
+        </div>
+        <LoginModal 
+          showCustomAlert={showCustomAlert}
+        />
+      </nav>
+      <AlertComponent 
+        variant={alertVariant}
+        message={alertMessage}
+      />
+    </>
+    );      
 }
 
