98aec0520da02ce23798b42e7f8154ac892d86e2
PlayJeri <nurmi.jeri@gmail.com>
2023-10-11 14:12:40 +0300

    moved modal state and login to modal component

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,44 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { LoginModal } from "./LoginModal";
 import { Link } from "react-router-dom";
+import { LoginModal } from "./LoginModal";
 import "../styles/NavBar.css"
 
 
 export const NavBar: React.FC = () => {
-    const [showLoginModal, setShowLoginModal] = useState(false);
-
-    const handleLoginClick = () => {
-        setShowLoginModal(true);
-    }
-
-    const handleModalClose = () => {
-        setShowLoginModal(false);
-    }
-
-    useEffect(() => {
-        const checkIfLoggedIn = async () => {
-            const accessToken = localStorage.getItem('access_token');
-            if (!accessToken) return setShowLoginModal(true);
-
-            const headers = {
-                Authorization: `Bearer ${accessToken}`,
-                'Content-Type': "application/json",
-            };
-
-            const response = await fetch('http://localhost:3000/auth/validate', {
-                 method: 'POST',
-                 headers: headers,
-            })
-
-            if (response.status !== 200) {
-                setShowLoginModal(true);
-            }
-        }
-
-        checkIfLoggedIn();
-    }, [])
-
-
     return (
         <nav className="navbar navbar-expand-lg bg-dark-subtle">
           <div className="container-fluid">
@@ -57,7 +22,7 @@ export const NavBar: React.FC = () => {
               </ul>
             </div>
           </div>
-          {showLoginModal && <LoginModal isOpen={showLoginModal} onClose={handleModalClose} />}
+          <LoginModal />
         </nav>
       );      
 }
