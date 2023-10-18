94f37114f105b02599bb24da8ca87b6dc5b20e79
PlayJeri <nurmi.jeri@gmail.com>
2023-10-18 16:33:31 +0300

    removed unnecessary function

--- frontend/src/containers/HomePage.tsx
+++ frontend/src/containers/HomePage.tsx
@@ -4,20 +4,14 @@ import "../styles/HomePage.css";
 import { Link } from "react-router-dom";
 
 
-const TicTacToeBoard: React.FC = () => {
-    return (
-        <div className="ttt-board-container">
-            <img src={TicTacToeImage} alt="Tic Tac Toe Board" className="ttt-image" />
-            <Link to="/game" className="play-button">PLAY</Link>
-        </div>
-    )
-}
-
 export const HomePage: React.FC = () => {
     return (
         <div className="home-page">
             <NavBar />
-            <TicTacToeBoard />
+            <div className="ttt-board-container">
+            <img src={TicTacToeImage} alt="Tic Tac Toe Board" className="ttt-image" />
+            <Link to="/game" className="play-button">PLAY</Link>
+            </div>
         </div>
     )
 }
