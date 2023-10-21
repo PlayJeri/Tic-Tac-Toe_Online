16054add5b03c8b951f4804cc3e2483c9847ff65
PlayJeri <nurmi.jeri@gmail.com>
2023-10-21 19:32:52 +0300

    Env variables and comments added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,24 +4,33 @@ import { upgrade } from './websocketController';
 import userRouter from './routers/userRouter';
 import authRouter from './routers/authRouter';
 import profileRouter from './routers/profileRouter';
+import dotenv from 'dotenv';
 
-const PORT = 3000;
+dotenv.config(); // Load environment variables
+
+const PORT = process.env.SERVER_PORT || 3000;
 const app = express();
 
-app.use(cors({ origin: "*" }));
+// CORS policy configurations
+app.use(cors({ origin: process.env.CORS_ORIGINS || "*" }));
 
+// Middleware
 app.use(express.json());
 
+// Routers
 app.use('/auth', authRouter);
 app.use('/profile', profileRouter);
 app.use('/user', userRouter);
 
+// Test endpoint (for development only)
 app.get('/', (req: Request, res: Response) => {
     res.send("Welcome to Tic Tac Toe Multiplayer!");
 })
 
+// Start the Express server on specific PORT
 const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
 
+// Pass the Express server to websocketController
 upgrade(server);
