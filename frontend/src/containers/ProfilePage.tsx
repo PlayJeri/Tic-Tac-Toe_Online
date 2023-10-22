94685772004e7e905a4de096c9eb7948f33a6ed0
PlayJeri <nurmi.jeri@gmail.com>
2023-10-22 20:18:33 +0300

    logic moved to helper function

--- frontend/src/containers/ProfilePage.tsx
+++ frontend/src/containers/ProfilePage.tsx
@@ -2,32 +2,18 @@ import { useEffect, useState } from 'react';
 import { ProfileInfo } from '../utils/types';
 import { ProfileInfoListComponent } from '../components/ProfileInfo';
 import { NavBar } from '../components/NavBar';
+import { getProfileData } from '../helpers/apiCommunicator';
 
 export const ProfilePage = () => {
     const [userData, setUserData] = useState<ProfileInfo | null>(null);
 
     useEffect(() => {
         const fetchData = async () => {
-        const accessToken = localStorage.getItem("access_token");
-        if (accessToken) {
-            const headers = {
-                Authorization: `Bearer ${accessToken}`,
-                'Content-Type': "application/json",
-            };
 
-            const response = await fetch('http://localhost:3000/profile', {
-                 method: 'GET',
-                 headers: headers,
-            })
-
-            if (!response.ok) {
-                throw new Error("Failed to fetch profile data.");
-            }
-            const data = await response.json();
-            setUserData(data);
+            const response = await getProfileData()
+            console.log(response)
+            setUserData(response);
         }
-    }
-
         fetchData();
     }, [])
 
