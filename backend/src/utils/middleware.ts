874b8c8f53b7e4e85833767a3c6085e6283a41ae
PlayJeri <nurmi.jeri@gmail.com>
2023-10-22 19:02:12 +0300

    Changed logic to use cookies

--- backend/src/utils/middleware.ts
+++ backend/src/utils/middleware.ts
@@ -1,22 +1,33 @@
 import { NextFunction, Response, Request } from "express";
 import { secretKey } from "../controllers/authControllers";
-import jwt from "jsonwebtoken";
+import { DecodedAccessToken, tokenPayload } from "./types";
+import jwt from 'jsonwebtoken';
 
 
+/**
+ * Middleware for validating an authentication token.
+ */
 export const validateTokenMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization?.replace("Bearer ", "");
+    // Extract the token from signed cookies
+    const token = req.signedCookies["auth_token"] as string;
 
-    if (!token) {
+    // Check if the token is missing or empty
+    if (!token || token.trim() === "") {
         return res.status(401).json({ error: "Unauthorized - Token not provided" });
     }
 
+    // Verify the token's authenticity using the provided secretKey
     jwt.verify(token, secretKey, (err, decoded) => {
         if (err) {
+            // If the token is invalid or expired, return an unauthorized response
             return res.status(401).json({ error: "Unauthorized - Invalid token" });
         }
 
-        req.decodedToken = decoded;
+        // If the token is valid, store its decoded payload in the response's locals
+        const tokenPayload = decoded as DecodedAccessToken;
+        res.locals.jwtData = tokenPayload;
 
+        // Continue with the next middleware or route handler
         next();
-    })
-}
+    });
+};
