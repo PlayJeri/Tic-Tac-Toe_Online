115a5d7ec401d4e4ea3370da4832653b4425a43a
PlayJeri <nurmi.jeri@gmail.com>
2023-10-26 11:33:31 +0300

    Documentation added

--- frontend/src/contextProviders/AuthenticationContextProvider.tsx
+++ frontend/src/contextProviders/AuthenticationContextProvider.tsx
@@ -1,46 +1,56 @@
 import { ReactNode, createContext, useContext, useEffect, useState } from "react";
 import { checkAuthStatus, loginUser, logoutUser, registerUser } from "../helpers/apiCommunicator";
 
+// Define the interface of the user object
 interface User {
     username: string;
     id: string;
 }
 
+// Define the interface of the authentication context
 interface UserAuth {
-    isLoggedIn: boolean;
-    user: User | null;
-    login: (username: string, password: string) => Promise<any>;
-    register: (username: string, password: string, retypePassword: string) => Promise<any>;
-    logout: () => Promise<any>;
+    isLoggedIn: boolean; // Indicates whether a user is authenticated or not
+    user: User | null; // Contains user information if authenticated, or null if not
+    login: (username: string, password: string) => Promise<any>; // Function to log in a user
+    register: (username: string, password: string, retypePassword: string) => Promise<any>; // Function to register a user
+    logout: () => Promise<any>; // Function to log out a user
 }
 
+// Create an authentication context with an initial value of null
 const AuthContext = createContext<UserAuth | null>(null);
 
+// Custom hook to access the authentication context
 export const useAuthContext = () => useContext(AuthContext);
 
+// AuthProvider component wraps the entire application
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
+    // State to manage user information and authentication status
     const [user, setUser] = useState<User | null>(null);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-
+    // UseEffect to check the authentication status when the component mounts
     useEffect(() => {
         async function checkStatus() {
             const data = await checkAuthStatus();
-            if (!data) return
-            setUser({ username: data.username, id: data.id });
-            setIsLoggedIn(true);
+            if (data) {
+                setUser({ username: data.username, id: data.id });
+                setIsLoggedIn(true);
+            }
         }
         checkStatus();
     }, []);
 
+    // Function to log in a user
     const login = async (username: string, password: string) => {
         const data = await loginUser(username, password);
-        if (!data) throw new Error("Login failed");
-
-        setUser({ username: data.username, id: data.id })
+        if (!data) {
+            throw new Error("Login failed");
+        }
+        setUser({ username: data.username, id: data.id });
         setIsLoggedIn(true);
     };
 
+    // Function to log out a user
     const logout = async () => {
         const data = await logoutUser();
         if (!data) {
@@ -48,24 +58,26 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
         }
         setUser(null);
         setIsLoggedIn(false);
-        window.location.reload();
+        window.location.reload(); // Refresh the page after logout
     };
 
+    // Function to register a user
     const register = async (username: string, password: string, retypePassword: string) => {
         try {
             const data = await registerUser(username, password, retypePassword);
-            login(username, password);
+            login(username, password); // Automatically log in after successful registration
 
+            // Redirect to the homepage after a brief delay
             setTimeout(() => {
                 window.location.replace("/");
-            }, 2000)
-            
+            }, 2000);
             return data;
         } catch (error) {
             throw error;
         }
     };
 
+    // Create the context value to provide to the context consumers
     const contextValue = {
         user,
         isLoggedIn,
@@ -74,9 +86,11 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
         register
     };
 
+    // Return the AuthProvider with the context value wrapping the children
     return (
         <AuthContext.Provider value={contextValue}>
             {children}
         </AuthContext.Provider>
     );
 };
+ 
--- frontend/src/contextProviders/WebSocketContext.tsx
+++ frontend/src/contextProviders/WebSocketContext.tsx
@@ -1,28 +1,34 @@
 import { ReactNode, createContext, useContext, useState, useEffect } from "react";
 import { checkAuthStatus } from "../helpers/apiCommunicator";
 
+// Define the interface for the WebSocket context
 interface WebSocketContextType {
-    webSocket: WebSocket | null;
-    setWebSocket: (ws: WebSocket | null) => void;
-    sendNewUserMessage: (username: string) => void;
+    webSocket: WebSocket | null; // The WebSocket connection instance
+    setWebSocket: (ws: WebSocket | null) => void; // A function to set the WebSocket instance
+    sendNewUserMessage: (username: string) => void; // A function to send a new user message over WebSocket
 }
 
+// Create a WebSocket context with an initial value of 'undefined'
 const WebSocketContext = createContext<WebSocketContextType | undefined>(undefined);
 
+// Custom hook to access the WebSocket context
 export const useWebSocketContext = () => {
     const context = useContext(WebSocketContext);
     if (!context) {
         throw new Error('useWebSocketContext must be used within a WebSocketProvider');
     }
-    return context
+    return context;
 }
 
+// WebSocketProvider component that wraps the entire application
 export const WebSocketProvider = ({ children }: { children: ReactNode }) => {
+    // State to manage the WebSocket connection
     const [webSocket, setWebSocket] = useState<WebSocket | null>(null);
 
+    // Function to send a new user message over WebSocket
     const sendNewUserMessage = (username: string) => {
         if (webSocket) {
-            console.log('send new user message');
+            console.log('Sending a new user message');
             const message = {
                 type: 'NEW_USER',
                 payload: {
@@ -33,24 +39,28 @@ export const WebSocketProvider = ({ children }: { children: ReactNode }) => {
         }
     };
 
+    // UseEffect to initialize WebSocket and send a message when the component mounts
     useEffect(() => {
+        // Function to check the user's authentication status
         async function checkStatus() {
-            const data = await checkAuthStatus();
-            if (!data) return
+            const data = await checkAuthStatus(); // Check user's authentication status
+            if (!data) return; // If not authenticated, return early
+            // Establish a WebSocket connection to the specified URL
             const wsConn = new WebSocket(`ws://localhost:3000`);
-            setWebSocket(wsConn);
-            sendNewUserMessage(data.username);          
+            setWebSocket(wsConn); // Set the WebSocket instance in the state
+            sendNewUserMessage(data.username); // Send a new user message with the authenticated username
         }
-        checkStatus();
+        checkStatus(); // Execute the checkStatus function when the component mounts
     }, []);
 
-    // Include the sendNewUserMessage function in the context value
+    // Create the context value to provide to the context consumers
     const contextValue = {
         webSocket,
         setWebSocket,
         sendNewUserMessage,
-    };   
+    };
 
+    // Return the WebSocketProvider with the context value wrapping the children
     return (
         <WebSocketContext.Provider value={contextValue}>
             {children}
