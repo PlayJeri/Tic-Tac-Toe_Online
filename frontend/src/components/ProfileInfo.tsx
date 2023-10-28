9c02fba63d4c9c826143f23b61477311fb629a74
PlayJeri <nurmi.jeri@gmail.com>
2023-10-28 17:30:32 +0300

    more info added

--- frontend/src/components/ProfileInfo.tsx
+++ frontend/src/components/ProfileInfo.tsx
@@ -2,7 +2,7 @@ import { Badge, ListGroup } from 'react-bootstrap';
 import { ProfileInfo } from '../utils/types';
 
 
-export const ProfileInfoListComponent: React.FC<ProfileInfo> = ({ wins, losses, secondsPlayed }) => {
+export const ProfileInfoListComponent: React.FC<ProfileInfo> = ({ wins, losses, draws, secondsPlayed }) => {
 
     const formatGameTime = (timePlayedSeconds: number): string =>  {
         const hours = Math.floor(timePlayedSeconds / 3600);
@@ -33,10 +33,42 @@ export const ProfileInfoListComponent: React.FC<ProfileInfo> = ({ wins, losses, 
         </ListGroup.Item>
         <ListGroup.Item as='li' className='d-flex justify-content-between align-items-center py-3'>
           <div className="fw-bold">
-            Win ratio
+            Draws
+          </div>
+          <Badge bg='primary' pill>
+            {draws}
+          </Badge>
+        </ListGroup.Item>
+        <ListGroup.Item as='li' className='d-flex justify-content-between align-items-center py-3'>
+          <div className="fw-bold">
+            Win Loss Ratio
           </div>
           <Badge bg='primary' pill>
             {(wins / losses).toFixed(2)}
+          </Badge>
+        </ListGroup.Item>
+        <ListGroup.Item as='li' className='d-flex justify-content-between align-items-center py-3'>
+          <div className="fw-bold">
+            Win %
+          </div>
+          <Badge bg='primary' pill>
+            {((wins / (wins + losses + draws)) * 100).toFixed(1)}%
+          </Badge>
+        </ListGroup.Item>
+        <ListGroup.Item as='li' className='d-flex justify-content-between align-items-center py-3'>
+          <div className="fw-bold">
+            Loss %
+          </div>
+          <Badge bg='primary' pill>
+            {((losses / (wins + losses + draws)) * 100).toFixed(1)}%
+          </Badge>
+        </ListGroup.Item>
+        <ListGroup.Item as='li' className='d-flex justify-content-between align-items-center py-3'>
+          <div className="fw-bold">
+            Draw %
+          </div>
+          <Badge bg='primary' pill>
+            {((draws / (wins + losses + draws)) * 100).toFixed(1)}%
           </Badge>
         </ListGroup.Item>
         <ListGroup.Item as='li' className='d-flex justify-content-between align-items-center py-3'>
