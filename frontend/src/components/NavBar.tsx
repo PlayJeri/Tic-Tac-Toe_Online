a0314c989d40b5956099082623c0b8aac47b012b
PlayJeri <nurmi.jeri@gmail.com>
2023-10-28 14:07:58 +0300

    Changed styles to react-bootstrap components

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { LoginModal } from "./LoginModal";
 import { AlertComponent } from "./Alert";
+import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import logo from "../assets/ttt-bg.png";
 
 
 export const NavBar: React.FC = () => {
@@ -17,25 +19,29 @@ export const NavBar: React.FC = () => {
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-dark-subtle">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/profile">Profile</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-          <LoginModal showCustomAlert={showCustomAlert}/>
-      </nav>
+    <Navbar expand="md" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand>
+            <img 
+              src={logo} 
+              style={{ maxWidth: '150px', maxHeight: '40px' }}
+            />
+            Tic-Tac-Toe
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
+            <Nav.Link as={Link} to={"/profile"}>Profile</Nav.Link>
+          </Nav>
+          <Nav className="ms-auto">
+            <Nav.Item>
+              <LoginModal showCustomAlert={showCustomAlert}/>
+            </Nav.Item>
+          </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       {showAlert ?
       <AlertComponent 
         variant={alertVariant}
